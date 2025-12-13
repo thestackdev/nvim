cde6c64c537b0e84029ffbd0af7c5272dfef85e4
thestackdev <shanmukeshwar@icloud.com>
2025-12-13 20:18:37 +0530

    Updates to lazyvim

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,13 +1,4 @@
 return {
   "folke/snacks.nvim",
-  opts = {
-    picker = {
-      sources = {
-        explorer = {
-          -- Include patterns take precedence over hidden/ignored/exclude
-          include = { ".env", ".env.*" },
-        },
-      },
-    },
-  },
+  opts = {},
 }
